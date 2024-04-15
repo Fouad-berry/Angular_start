5a2fc9aec4c4408af2dd3adc6735c67760e0b689
Fouad-berry <fidriss96@gmail.com>
2024-04-15 11:13:35 +0200

    angular installation commit

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ class HelloWorld extends HTMLElement {
     constructor(){
         super();
         this.innerHTML = '<p>Hello world!</p>';
-    }
+
+        if(this.className == 'f-end'){
+            this.innerHTML='<p style="font-weight:bold;">Bonjour le monde</p>';
+        }
+  }
 }
 
 customElements.define('hello-world', HelloWorld);
---
+++ readme.md
@@ -0,0 +1,2 @@
+Download Angular : npm i @angular/cli@16.2.14
+know angular version : ng version
