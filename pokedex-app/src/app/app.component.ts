a8635efd1263123726d6331caea0c0b0799f0151
Fouad-berry <fidriss96@gmail.com>
2024-04-16 10:08:49 +0200

    maj commit

--- pokedex-app/src/app/app.component.ts
+++ pokedex-app/src/app/app.component.ts
@@ -34,54 +34,7 @@ import { Pokemon } from './pokemon.models';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div class="search-container">
-    <input class="search" type="text" placeholder="Rechercher un Pokémon" [(ngModel)]="searchTerm" (input)="filterPokemon()">
-  <div>
-    <div class="pokemon-list">
-      <div class="pokemon-card" *ngFor="let pokemon of filteredPokemonList">
-        <img [src]="pokemon.picture" alt="{{ pokemon.name }}">
-        <h2>{{ pokemon.name }}</h2>
-        <p>Type: {{ pokemon.types }}</p>
-        <p>Date de création: {{ pokemon.created }}</p>
-      </div>
-    </div>
-  `,
-  styles: [`
-  .search-container {
-    text-align: center;
-  }
-  .search {
-    width: 25%;
-    margin: 20px auto;
-    padding: 10px;
-    font-size: 16px;
-    border: 2px solid #ccc;
-    border-radius: 5px;
-    outline: none;
-  }
-  
-  .search::placeholder {
-    color: #999;
-  }
-    .pokemon-list {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-around;
-    }
-    .pokemon-card {
-      width: 30%;
-      margin-bottom: 20px;
-      border: 1px solid #ccc;
-      background-color: #fff;
-      text-align: center;
-    }
-    img {
-      max-width: 100%;
-      height: auto;
-    }
-  `]
-})
+  templateUrl: `./app.component.html`})
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
   filteredPokemonList: Pokemon[] = [];
